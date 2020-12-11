ed5a47003fb31a692a5a9661918d862c97f9c083
good-intentions <72018474+good-intentions@users.noreply.github.com>
2020-12-11 09:57:13 +0200

    Add files via upload

---
+++ MyPointTest.java
@@ -0,0 +1,15 @@
+package simeon;
+
+public class MyPointTest {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			MyPoint a = new MyPoint();
+			MyPoint b = new MyPoint(17, 43.5);
+			
+			
+			System.out.println(a.distance(b));
+		}
+
+	}
