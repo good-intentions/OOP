fa6b47f56ced6fdab5fa0e4c41ba2ee454b9c4a8
good-intentions <72018474+good-intentions@users.noreply.github.com>
2020-12-11 09:56:49 +0200

    Add files via upload

---
+++ MyIntegerTest.java
@@ -0,0 +1,16 @@
+package simeon;
+
+public class MyIntegerTest {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			MyInteger x = new MyInteger(17);
+			System.out.println(x.getValue());
+			System.out.println(x.isEven());
+			System.out.println(x.isOdd());
+			System.out.println(x.isPrime());
+			System.out.println(x.equals(17));
+			
+		}
+
+	}
